c6f2e4018b8dd7fc753650130e23b0ccc4578762
Priyanka <91836065+GoodVibes123@users.noreply.github.com>
2022-01-21 17:22:46 +0530

    Add files via upload

---
+++ assignment 2 input.py
@@ -0,0 +1,112 @@
+#Question 1
+
+print("Answer 1.")
+
+questionstring = "Python is a case sensitive language"
+
+#part a
+#printing the length of the string
+print("a.",len(questionstring))
+
+#part b
+#print the string in reverse order
+print("b.",questionstring[::-1])
+
+#part c
+#store a substring into a new string
+slicedstring = questionstring[10:]
+print("c.",slicedstring)
+
+#part d
+#replacing a substring with another
+replacedstring = questionstring.replace("a case sensitive","object oriented")
+print("d.",replacedstring)
+
+#part e
+#printing the index of "a"
+print("e.",questionstring.index("a"))
+
+#part f
+#printing the string wihtout the whitespaces
+stringlist = questionstring.split()
+print("f. ",*stringlist, sep="")
+
+#_____________________________________________________________
+
+#Question 2
+
+print("\n\nAnswer 2.")
+
+#storing details in variables
+name= "Priyanka Soni"
+sid= "21103051"
+department= "CSE"
+cgpa= "10"
+
+#printing the desired output with the format function
+print("Hey, {} Here!".format(name))
+print("My SID is {}".format(sid))
+print("I am from {} department and my CGPA is {}".format(department, cgpa))
+
+#_____________________________________________________________
+
+#Question 3
+
+print("\n\nAnswer 3.")
+
+#defining values
+a=56
+b=10
+
+#printing answer outputs
+print("a.",a&b)
+print("b.",a|b)
+print("c.",a^b)
+print("d. New values of a and b are ", a<<2, "and", b<<2)
+print("e. New values of a and b are ", a>>2, "and", b>>2)
+
+#_____________________________________________________________
+
+#Question 4
+
+print("\n\nAnswer 4.")
+
+#inputing 3 values
+inputlist= list(map(int,input("Input 3 values:").split()))
+
+#finding the greatest value
+greatestvalue= inputlist[0]
+for i in range(3):
+    if inputlist[i] > greatestvalue:
+        greatestvalue = inputlist[i]
+print("Greatest value is ",greatestvalue)
+
+#_____________________________________________________________
+
+#Question 5
+
+print("\n\nAnswer 5.")
+
+#inputing string
+inputstring = input("Enter a string:")
+
+if "name" in inputstring:
+    print("Yes")
+else:
+    print("No")
+
+#_____________________________________________________________
+
+#Question 6
+
+print("\n\nAnswer 6.")
+
+sides = list(map(int,input("Input the length of 3 sides:").split()))
+
+#test to check if the sides can make a triangle
+if sides[0] + sides[1] > sides[2] and\
+    sides[2] + sides[0] > sides[1] and\
+    sides[2] + sides[1] > sides[0]:
+    print("Yes")
+else:
+    print("No")
---
+++ assignment 2 output.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================= RESTART: C:/python projects/s.py =======================
+Answer 1.
+a. 35
+b. egaugnal evitisnes esac a si nohtyP
+c. a case sensitive language
+d. Python is object oriented language
+e. 10
+f. Pythonisacasesensitivelanguage
+
+
+Answer 2.
+Hey, Priyanka Soni Here!
+My SID is 21103051
+I am from CSE department and my CGPA is 10
+
+
+Answer 3.
+a. 8
+b. 58
+c. 50
+d. New values of a and b are  224 and 40
+e. New values of a and b are  14 and 2
+
+
+Answer 4.
+Input 3 values:75 3000 257
+Greatest value is  3000
+
+
+Answer 5.
+Enter a string: My name is Priyanka.
+Yes
+
+
+Answer 6.
+Input the length of 3 sides:36 75 48
+Yes
